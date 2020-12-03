ae90aaae480d9240b7e72e097aa0568b6b1d6e5b
Marco Brescianini <marco.brescianini@bandyer.com>
2020-12-03 22:05:59 +0100

    Project inception

---
+++ Source/BandyerTestKit.h
@@ -0,0 +1,12 @@
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BandyerTestKit.
+FOUNDATION_EXPORT double BandyerTestKitVersionNumber;
+
+//! Project version string for BandyerTestKit.
+FOUNDATION_EXPORT const unsigned char BandyerTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BandyerTestKit/PublicHeader.h>
+
+
