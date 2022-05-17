88393828285f26da7bc09edea79bddf3697709a3
Marco Brescianini <marco.brescianini@bandyer.com>
2022-05-17 10:47:41 +0200

    Fix missing FBSnapshotTestCase extension

--- Source/Snapshot/SnapshotTestCase.swift
+++ Source/Snapshot/SnapshotTestCase.swift
@@ -88,3 +88,55 @@ open class SnapshotTestCase: FBSnapshotTestCase {
         verifySnapshot(viewController, devices: devices, variants: variants, compatibleWithUIDevice: .current, compatibleWithUIScreen: .main, tolerance: tolerance, identifier: identifier, file: file, line: line)
     }
 }
+
+private extension FBSnapshotTestCase {
+
+    func FBSnapshotVerifyView(_ view: UIView, identifier: String? = nil, suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), perPixelTolerance: CGFloat = 0, overallTolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
+        FBSnapshotVerifyViewOrLayer(view, identifier: identifier, suffixes: suffixes, perPixelTolerance: perPixelTolerance, overallTolerance: overallTolerance, file: file, line: line)
+    }
+
+    private func FBSnapshotVerifyViewOrLayer(_ viewOrLayer: AnyObject, identifier: String? = nil, suffixes: NSOrderedSet = FBSnapshotTestCaseDefaultSuffixes(), perPixelTolerance: CGFloat = 0, overallTolerance: CGFloat = 0, file: StaticString = #file, line: UInt = #line) {
+        let envReferenceImageDirectory = self.getReferenceImageDirectory(withDefault: nil)
+        let envImageDiffDirectory = self.getImageDiffDirectory(withDefault: nil)
+        var error: NSError?
+        var comparisonSuccess = false
+
+        for suffix in suffixes {
+            let referenceImagesDirectory = "\(envReferenceImageDirectory)\(suffix)"
+            let imageDiffDirectory = envImageDiffDirectory
+            if viewOrLayer.isKind(of: UIView.self) {
+                do {
+                    try compareSnapshot(of: viewOrLayer as! UIView, referenceImagesDirectory: referenceImagesDirectory, imageDiffDirectory: imageDiffDirectory, identifier: identifier, perPixelTolerance: perPixelTolerance, overallTolerance: overallTolerance)
+                    comparisonSuccess = true
+                } catch let error1 as NSError {
+                    error = error1
+                    comparisonSuccess = false
+                }
+            } else if viewOrLayer.isKind(of: CALayer.self) {
+                do {
+                    try compareSnapshot(of: viewOrLayer as! CALayer, referenceImagesDirectory: referenceImagesDirectory, imageDiffDirectory: imageDiffDirectory, identifier: identifier, perPixelTolerance: perPixelTolerance, overallTolerance: overallTolerance)
+                    comparisonSuccess = true
+                } catch let error1 as NSError {
+                    error = error1
+                    comparisonSuccess = false
+                }
+            } else {
+                assertionFailure("Only UIView and CALayer classes can be snapshotted")
+            }
+
+            assert(recordMode == false, message: "Test ran in record mode. Reference image is now saved. Disable record mode to perform an actual snapshot comparison!", file: file, line: line)
+
+            if comparisonSuccess || recordMode {
+                break
+            }
+
+            assert(comparisonSuccess, message: "Snapshot comparison failed: \(String(describing: error))", file: file, line: line)
+        }
+    }
+
+    func assert(_ assertion: Bool, message: String, file: StaticString, line: UInt) {
+        if !assertion {
+            XCTFail(message, file: file, line: line)
+        }
+    }
+}
