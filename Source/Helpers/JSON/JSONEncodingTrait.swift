71dad75931959916245292bdea72c42096b029f1
Alessandro Limardo <alessandro.limardo@kaleyra.com>
2022-10-05 15:16:55 +0200

    Add encode static functions to JSONEncodingTrait protocol

--- Source/Helpers/JSON/JSONEncodingTrait.swift
+++ Source/Helpers/JSON/JSONEncodingTrait.swift
@@ -11,22 +11,40 @@ public protocol JSONEncodingTrait {
     func encode<T: Encodable>(_ value: T, outputFormatting: JSONEncoder.OutputFormatting) throws -> String
 
     func encode<T: Encodable>(_ value: T, encoder: JSONEncoder) throws -> String
+
+    static func encode<T: Encodable>(_ value: T) throws -> String
+
+    static func encode<T: Encodable>(_ value: T, outputFormatting: JSONEncoder.OutputFormatting) throws -> String
+
+    static func encode<T: Encodable>(_ value: T, encoder: JSONEncoder) throws -> String
 }
 
 @available(iOS 11.0, *)
 public extension JSONEncodingTrait {
 
     func encode<T: Encodable>(_ value: T) throws -> String {
-        try encode(value, outputFormatting: [.prettyPrinted, .sortedKeys])
+        try Self.encode(value)
     }
 
     func encode<T: Encodable>(_ value: T, outputFormatting: JSONEncoder.OutputFormatting) throws -> String {
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = outputFormatting
-        return try encode(value, encoder: encoder)
+        try Self.encode(value, outputFormatting: outputFormatting)
     }
 
     func encode<T: Encodable>(_ value: T, encoder: JSONEncoder) throws -> String {
+        try Self.encode(value, encoder: encoder)
+    }
+
+    static func encode<T: Encodable>(_ value: T) throws -> String {
+        try Self.encode(value, outputFormatting: [.prettyPrinted, .sortedKeys])
+    }
+
+    static func encode<T: Encodable>(_ value: T, outputFormatting: JSONEncoder.OutputFormatting) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = outputFormatting
+        return try Self.encode(value, encoder: encoder)
+    }
+
+    static func encode<T: Encodable>(_ value: T, encoder: JSONEncoder) throws -> String {
         let data = try encoder.encode(value)
         return String(data: data, encoding: .utf8)!
     }
