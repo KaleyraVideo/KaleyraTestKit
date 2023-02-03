d9130102bffe88663122a652c8a304a2d107f4b5
Vadim <vadim.shalugin@kaleyra.com>
2023-02-03 15:03:20 +0100

    Update EnumMatchers to check associated value

--- Source/Extensions/SwiftHamcrest/Matchers/EnumMatchers.swift
+++ Source/Extensions/SwiftHamcrest/Matchers/EnumMatchers.swift
@@ -20,3 +20,20 @@ public func matchCase<T>(_ expected: T) -> Matcher<T> {
     }
 }
 
+public func matchCaseWithAssociatedValue<T: Equatable>(_ expected: T) -> Matcher<T> {
+
+    func caseName<T>(_ value: T) -> String {
+        return Mirror(reflecting: value).children.first?.label ?? String(describing: value)
+    }
+
+    return Matcher("\(expected) case") { (actual: T) -> MatchResult in
+        guard Mirror(reflecting: actual).displayStyle == .enum else { return .mismatch("Actual value should be an enum, got \(actual)") }
+        guard Mirror(reflecting: expected).displayStyle == .enum else { return .mismatch("Expected value should be an enum, got \(expected)") }
+
+        let actualCase = caseName(actual)
+        let expectedCase = caseName(expected)
+        let isMatching = actual == expected && actualCase == expectedCase
+        return isMatching ? .match : .mismatch("\(actualCase) does not match \(expectedCase)")
+    }
+}
+
