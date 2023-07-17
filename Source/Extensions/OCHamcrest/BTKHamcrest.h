ee29730747b8515f06ea97d510c8ee3ba58d2465
Marco Brescianini <marco.brescianini@kaleyra.com>
2023-07-17 08:57:24 +0200

    Delete unused OCHamcrest assertions

--- Source/Extensions/OCHamcrest/BTKHamcrest.h
+++ Source/Extensions/OCHamcrest/BTKHamcrest.h
@@ -1,7 +1,5 @@
 // Copyright © 2018-2022 Kaleyra S.p.a. All Rights Reserved.
 // See LICENSE.txt for licensing information
 
-#import <BandyerTestKit/BTKGeometryAssert.h>
-#import <BandyerTestKit/BTKIsEqualToGeometryValue.h>
 #import <BandyerTestKit/BTKThrowsException.h>
 #import <BandyerTestKit/BTKThreadAssert.h>
