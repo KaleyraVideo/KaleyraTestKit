ac53df2be304af93344afbe491fdc2a11692cf52
Marco Brescianini <marco.brescianini@kaleyra.com>
2023-07-17 14:43:11 +0200

    Add loremIpsum properties to String extension

--- Source/Helpers/Foundation/String/String+Fixture.swift
+++ Source/Helpers/Foundation/String/String+Fixture.swift
@@ -15,4 +15,12 @@ public extension String {
     static let foo = "foo"
     static let bar = "bar"
     static let foobar = "foobar"
+
+    static var loremIpsum: String {
+        "Lorem ipsum dolor sit amet"
+    }
+
+    static var loremIpsumParagraph: String {
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros est, egestas id vehicula eu, lobortis eu massa. Suspendisse potenti. Fusce ante magna, volutpat eu tortor in, aliquam cursus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis nulla quis interdum tincidunt. Suspendisse sollicitudin sed quam at dictum. Proin sagittis, odio nec suscipit aliquam, mi mauris mattis lectus, feugiat tristique dolor turpis ut erat. Sed eget ullamcorper diam. Donec pellentesque accumsan tortor, a condimentum neque consectetur vel. Duis suscipit elit eros, at vehicula sapien eleifend in. Donec pretium nibh vel odio rhoncus luctus. Proin aliquet diam nec justo blandit sagittis. Nunc mattis rutrum turpis, in rutrum magna"
+    }
 }
