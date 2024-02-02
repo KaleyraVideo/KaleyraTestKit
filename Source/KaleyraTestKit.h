10526afe8e090948d63852e9241752f9ec1363ee
Marco Brescianini <136689400+marcobrescianini-kaleyra@users.noreply.github.com>
2024-02-02 18:19:57 +0100

    Remove xcode project and info plists

--- Source/KaleyraTestKit.h
+++
@@ -1,7 +0,0 @@
-// Copyright © 2018-2022 Kaleyra S.p.a. All Rights Reserved.
-// See LICENSE.txt for licensing information
-
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double KaleyraTestKitVersionNumber;
-FOUNDATION_EXPORT const unsigned char KaleyraTestKitVersionString[];
